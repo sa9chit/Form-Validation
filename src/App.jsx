7efbe84fb141996c4247eb158139e7a1ff7fb679
sa9chit <gaurshalini78@gmail.com>
2025-05-20 02:45:22 +0530

    edit

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,75 @@
+import { useEffect, useRef, useState } from "react";
 import "./App.css";
-import { InputType } from "./component/input";
-import { Btn } from "./component/btn";
-import { FormDriven } from "./component/data";
-import { useState } from "react";
-const App = () => {
-  let [state, setState] = useState({
-    name: "",
-    email: "",
-    gender: "",
-    country: "",
-    limitation: "",
-    feedback: "",
-  });
-  console.log(state);
+
+function App() {
+  const inpREF = useRef({});
+  const [hold, setHold] = useState([]);
+  const [editIndex, setEditIndex] = useState(null);
+  const handleCLick = () => {
+    const form = {};
+    Object.keys(inpREF.current).forEach((val) => {
+      const values = inpREF.current[val].value;
+
+      form[val] = values;
+      inpREF.current[val].value = "";
+    });
+    if (editIndex !== null) {
+      setHold((prev) => prev.map((val, i) => (editIndex === i ? form : val)));
+
+      setEditIndex(null);
+    } else {
+      setHold((prev) => [...prev, form]);
+    }
+  };
+
+  useEffect(() => {
+    if (editIndex != null) {
+      let contain = hold[editIndex];
+      Object.keys(inpREF.current).forEach((val) => {
+        inpREF.current[val].value = contain[val];
+      });
+    }
+  }, [editIndex]);
+
   return (
     <div>
-      {FormDriven.map((val, i) => {
-        return (
-          <div key={i + val.max * val.min}>
-            <InputType
-              setState={setState}
-              state={state}
-              type={val.type}
-              min={val.min}
-              max={val.max}
-              placeholder={val.placeholder}
-              option={val.value}
-            />
-          </div>
-        );
-      })}
+      <input
+        ref={(e) => (inpREF.current["name"] = e)}
+        className="inp"
+        type="text"
+        placeholder="enter value"
+      ></input>
+      <input
+        ref={(e) => (inpREF.current["id"] = e)}
+        className="inp"
+        type="text"
+        placeholder="enter value"
+      ></input>
+      <button
+        onClick={handleCLick}
+        className="bg-red-500 px-7 py-1 rounded-2xl ml-4 font-semibold text-white"
+      >
+        Submit
+      </button>
+
+      <div className="">
+        {hold.map((values, i) => {
+          return (
+            <div key={i} className="flex  gap-4 border w-fit px-5 ml-4 mt-10 ">
+              <div>{values.name}</div> |<div>{values.id}</div> |
+              <button
+                onClick={() => {
+                  setEditIndex(i);
+                }}
+              >
+                Edit
+              </button>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
-};
+}
 
 export default App;
--- src/component/btn.jsx
+++
@@ -1,9 +0,0 @@
-export const Btn = ({ btn }) => {
-  return (
-    <>
-      <button className="bg-amber-700 text-white font-semibold px-4 rounded-2xl">
-        {btn}
-      </button>
-    </>
-  );
-};
